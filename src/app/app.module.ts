7e042d5aa7e705ccc455f988a83886db00a13786
Virgilio mutondo <vmutondo@gmail.com>
2020-11-13 15:47:37 +0200

    add bootstrap and material angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule,
+  MatChipsModule, MatDatepickerModule, MatDialogModule, MatExpansionModule, MatGridListModule, MatIconModule,
+  MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule,
+  MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+  MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule,
+  MatToolbarModule, MatTooltipModule, MatStepperModule, MatFormFieldModule, MatOptionModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,44 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatPaginatorModule,
+    MatStepperModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatOptionModule,
+    MatMenuModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
